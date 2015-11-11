88b198680fdb8344b0747f9c9dfda3eb5671ba8f
Brett Wise <brettdwise@gmail.com>
2015-11-11 17:53:43 -0500

    Added comeonin under applications so that it would be managed by OTP

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Pxblog.Mixfile do
   def application do
     [mod: {Pxblog, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :comeonin]]
   end
 
   # Specifies which paths to compile per environment
