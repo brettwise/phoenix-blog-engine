73f4f47d64d68d8d4aa18ce0b82189922f7b069c
Brett Wise <brettdwise@gmail.com>
2015-11-11 16:07:18 -0500

    Used generator to add users. Added user resource to routes

--- web/router.ex
+++ web/router.ex
@@ -18,6 +18,7 @@ defmodule Pxblog.Router do
 
     get "/", PageController, :index
     resources "/posts", PostController
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
