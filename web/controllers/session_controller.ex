41ce64a0f77f50988eb8f6fd4d02a7d1f6c10eab
Brett Wise <brettdwise@gmail.com>
2015-11-16 11:30:12 -0500

    Various bug fixes and things I had over looked

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -9,10 +9,9 @@ defmodule Pxblog.SessionController do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) when not is_nil(username) and not is_nil(password) do
-    user = Repo.get_by(User, username: user_params["username"])
-    user
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}}) when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
   end
 
   def create(conn, _) do
@@ -27,7 +26,7 @@ defmodule Pxblog.SessionController do
     |> halt()
   end
 
-  defp sign_in(user, password, conn) when is_nil(user) do
+  defp sign_in(user, _password, conn) when is_nil(user) do
     failed_login(conn)
   end
 
@@ -35,7 +34,8 @@ defmodule Pxblog.SessionController do
     if checkpw(password, user.password_digest) do
       conn
       |> put_session(:current_user, %{id: user.id, username: user.username})
-      |> put_flash(to: page_path(conn, :index))
+      |> put_flash(:info, "Sign in successful!")
+      |> redirect(to: page_path(conn, :index))
     else
       failed_login(conn)
     end
@@ -46,19 +46,5 @@ defmodule Pxblog.SessionController do
       |> delete_session(:current_user)
       |> put_flash(:info, "Signed out successfully!")
       |> redirect(to: page_path(conn, :index))
-  end
-
-  defp sign_in(user, password, conn) do
-    if checkpw(password, user.password_digest) do
-      conn
-      |> put_session(:current_user, %{id: user.id, username: user.username})
-      |> put_flash(:info, "Sign in successful!")
-      |> redirect(to: page_path(conn, :index))
-    else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
-    end
   end
 end
